3506fdc1a91d45f8416d1948e96a6dbbdb64a9b3
elainees-devs <elainees.devs@gmail.com>
2025-09-02 12:50:37 +0300

    Add user registration view with UserCreationForm and login integration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library,Book
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 # Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto login after registration
+            return redirect("list_books")
+    else:
+        form = UserCreationForm()
+    return render(request, "relationship_app/register.html", {"form": form})
 
 # Class-based view: library detail
 class LibraryDetailView(DetailView):
