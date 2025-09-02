d2eae296a0ebf8fd7273a85a82da37851ea573b8
elainees-devs <elainees.devs@gmail.com>
2025-09-02 14:35:50 +0300

    Updated the order of imports from django.contrib.auth.decorators

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic.detail import DetailView
 from .models import Library, Book, Author
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required, user_passes_test
 
 
 # Add Book
