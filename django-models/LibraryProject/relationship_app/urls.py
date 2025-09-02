a2330fa01eb35406f2e364a03c8e945afcf58c1f
elainees-devs <elainees.devs@gmail.com>
2025-09-02 12:54:27 +0300

    Update urls.py to use views.register explicitly in authentication URL patterns

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import list_books, LibraryDetailView, register
 from . import views
 
-
 urlpatterns = [
-    path("books/", list_books, name="list_books"),  # function-based
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based
+    path("books/", views.list_books, name="list_books"),
+    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),
 
-
-# Auth routes
+    # Auth routes
     path("login/", auth_views.LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", register, name="register"),
+    path("register/", views.register, name="register"),
 ]
