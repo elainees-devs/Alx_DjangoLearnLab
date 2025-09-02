e45299c011db9f6042758fbaa4cb15f6ee8fa81c
elainees-devs <elainees.devs@gmail.com>
2025-09-02 11:47:49 +0300

    Remove other classes created and reverted to original

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,23 +1,10 @@
 from django.db import models
 
-
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, name='books')
+    author = models.CharField(max_length=200)
     publication_year = models.IntegerField()
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
-    
-class Librar(models.Model):
-    name = models.CharField(max_length=100)
-    books=models.ManyToManyField(Book, related_name='libraries')
-
-    def __str__(self):
-        return self.name
 
