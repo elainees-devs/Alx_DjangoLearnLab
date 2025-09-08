0b505a2b4eda3eb46395590e6e074645a1ed5d8f
elainees-devs <elainees.devs@gmail.com>
2025-09-08 14:06:02 +0300

    Fix models.py: remove duplicate methods, clean up CustomUserManager, connect profile_upload_to, and ensure custom permissions (can_view, can_create, can_edit, can_delete) are defined in Book model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -2,15 +2,23 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publication_year = models.IntegerField()
 
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
-    
+
+
 def profile_upload_to(instance, filename):
     return f"profiles/{instance.username}/{filename}"
 
@@ -45,21 +53,10 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(username, email, password, **extra_fields)
 
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        if not username:
-            raise ValueError("The username must be set")
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-     
-
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to="profiles/", null=True, blank=True)
+    profile_photo = models.ImageField(upload_to=profile_upload_to, null=True, blank=True)
 
     objects = CustomUserManager()
 
@@ -67,5 +64,3 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
-
-
