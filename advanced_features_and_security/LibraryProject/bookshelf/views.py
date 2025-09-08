0a0b7b43af1b1d442975b6356631f01de28e24fa
elainees-devs <elainees.devs@gmail.com>
2025-09-08 14:31:28 +0300

    Add ExampleForm and refactor book_create/book_edit views to use it

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm 
 
 # View: List all books (requires can_view)
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -9,31 +10,30 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, "books/book_list.html", {"books": books})
 
-
-# ➕ View: Create a new book (requires can_create)
+# View: Create a new book (requires can_create)
 @permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
     if request.method == "POST":
-        title = request.POST.get("title")
-        author = request.POST.get("author")
-        year = request.POST.get("publication_year")
-        Book.objects.create(title=title, author=author, publication_year=year)
-        return redirect("book_list")
-    return render(request, "books/book_form.html")
-
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = ExampleForm()
+    return render(request, "books/book_form.html", {"form": form})
 
 # View: Edit an existing book (requires can_edit)
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == "POST":
-        book.title = request.POST.get("title")
-        book.author = request.POST.get("author")
-        book.publication_year = request.POST.get("publication_year")
-        book.save()
-        return redirect("book_list")
-    return render(request, "books/book_form.html", {"book": book})
-
+        form = ExampleForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect("book_list")
+    else:
+        form = ExampleForm(instance=book)
+    return render(request, "books/book_form.html", {"form": form})
 
 # View: Delete a book (requires can_delete)
 @permission_required('bookshelf.can_delete', raise_exception=True)
