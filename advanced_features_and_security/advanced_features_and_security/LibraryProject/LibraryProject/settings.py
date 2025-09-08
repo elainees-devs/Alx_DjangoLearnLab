14c45a119d40d235b4838ce0b7746456491b907e
elainees-devs <elainees.devs@gmail.com>
2025-09-08 12:37:37 +0300

    Update INSTALLED_APPS to include LibraryProject.relationship_app

--- advanced_features_and_security/advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
-    'LibraryProject.relationship_app',
+    'relationship_app',
     'users'
 ]
 
