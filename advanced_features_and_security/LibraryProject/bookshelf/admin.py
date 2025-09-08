cdaeb9323bffd737af12ca1612fe3c9549c72f2b
elainees-devs <elainees.devs@gmail.com>
2025-09-08 13:58:00 +0300

    Register CustomUser model in Django admin with CustomUserAdmin to manage date_of_birth and profile_photo fields

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 from .models import Book
 
 # Customize how the Book model appears in admin  interface
@@ -8,6 +10,20 @@ class BookAdmin(admin.ModelAdmin):
     search_fields=("title", "author")
 
 
+class CustomUserAdmin(UserAdmin):
+    """
+    Extends Django's built-in UserAdmin to display custom fields.
+    """
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_superuser")
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+
 # Register the model with its custom admin configuration
-admin.site.register(Book, BookAdmin)
+admin.site.register(Book, BookAdmin, CustomUser, CustomUserAdmin)
 
