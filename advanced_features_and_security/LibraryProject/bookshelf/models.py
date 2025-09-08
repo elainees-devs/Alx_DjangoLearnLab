34087253a4a72811743907535a922b11b67d0190
elainees-devs <elainees.devs@gmail.com>
2025-09-08 13:29:52 +0300

    Add CustomUser model with manager, date_of_birth, and profile_photo

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
+
+
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -7,4 +10,41 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+    
+def profile_upload_to(instance, filename):
+    return f"profiles/{instance.username}/{filename}"
+
+
+class CustomUserManager(DjangoUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The username must be set")
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to=profile_upload_to, null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    REQUIRED_FIELDS = ["email"]  # keeps DOB optional
+
+    def __str__(self):
+        return self.username
 
