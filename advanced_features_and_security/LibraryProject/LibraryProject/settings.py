124c4babf77adb6c6a5c10b57feba2aae7a2694a
elainees-devs <elainees.devs@gmail.com>
2025-09-08 13:49:06 +0300

    Configure AUTH_USER_MODEL to use bookshelf.CustomUser

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
     }
 }
 
+# Custom user model
+AUTH_USER_MODEL = "bookshelf.CustomUser"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
