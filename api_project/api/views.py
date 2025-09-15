15fbd1f3ecac6d47e4da315148446bf3149f59d1
elainees-devs <elainees.devs@gmail.com>
2025-09-15 10:46:39 +0300

    Add DRF permission classes to BookList and BookViewSet for authenticated access

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,14 +1,19 @@
 # api/views.py
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Only logged-in users can view
+
 
 
 # ViewSet for full CRUD
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  
+    # Authenticated → full CRUD
+    # Unauthenticated → read-only (GET)
