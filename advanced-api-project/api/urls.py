9cea4a8618d2754ce9f3c41b411a4e3315a26ebf
elainees-devs <elainees.devs@gmail.com>
2025-09-28 14:15:25 +0300

    add modified URL patterns for updating and deleting books

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path("books/", BookListView.as_view(), name="book-list"),                  # GET all books
     path("books/<int:pk>/", BookDetailView.as_view(), name="book-detail"),     # GET one book by ID
     path("books/create/", BookCreateView.as_view(), name="book-create"),       # POST create a new book
-    path("books/<int:pk>/update/", BookUpdateView.as_view(), name="book-update"),  # PUT/PATCH update
-    path("books/<int:pk>/delete/", BookDeleteView.as_view(), name="book-delete"),  # DELETE
+    path("books/update/<int:pk>/", BookUpdateView.as_view(), name="book-update"),   # PUT/PATCH update
+    path("books/delete/<int:pk>/", BookDeleteView.as_view(), name="book-delete"),  # DELETE
 ]
