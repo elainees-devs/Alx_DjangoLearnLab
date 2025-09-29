a40daf77753e182706ab416651f4f8dfb79bb3c5
elainees-devs <elainees.devs@gmail.com>
2025-09-29 18:42:28 +0300

    Add TaggableManager to Post model to enable tagging functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 #blog/models.py
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 
@@ -13,6 +14,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name="posts"   # allows user.posts.all()
     )
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
