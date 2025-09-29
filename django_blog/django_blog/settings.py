4a701a3804a4a23c6ac7a8fcb078ad52f7004ac8
elainees-devs <elainees.devs@gmail.com>
2025-09-29 10:36:49 +0300

    Add configuration for static files

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -113,6 +113,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
