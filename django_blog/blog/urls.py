7a05e0bf9dfbc1b081faa3f920398a367bd2e5bd
elainees-devs <elainees.devs@gmail.com>
2025-09-29 18:26:31 +0300

    Update comments urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 
     # Comment URLs
-    path('posts/<int:post_pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
 ]
