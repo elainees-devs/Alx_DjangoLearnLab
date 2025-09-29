2a336061022c2ace882525ac82026d7668ee2276
elainees-devs <elainees.devs@gmail.com>
2025-09-29 10:26:48 +0300

    Add URL patterns for register, login, logout, and profile views

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+path('register/', views.register, name='register'),
+path('login/', views.CustomLoginView.as_view(), name='login'),
+path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+path('profile/', views.profile_view, name='profile'),
+]
