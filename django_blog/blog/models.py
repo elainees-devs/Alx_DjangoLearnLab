3f8c5c57a055db2a71f049c95b558efa6d6b85d4
elainees-devs <elainees.devs@gmail.com>
2025-09-29 10:22:27 +0300

    Add Profile model with bio and avatar linked to User

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -15,3 +16,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+
+
+    def __str__(self):
+        return f"Profile({self.user.username})"
