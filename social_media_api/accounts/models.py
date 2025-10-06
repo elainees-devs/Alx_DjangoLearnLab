a37a9227230570e18575396fbf3b6e952390e88e
elainees-devs <elainees.devs@gmail.com>
2025-10-06 20:15:36 +0300

    refactor: Rename profile to profile_picture

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     bio=models.TextField(blank=True)
-    profile=models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    profile_picture=models.ImageField(upload_to='profile_picture/', blank=True, null=True)
     followers=models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
 
     def __str__(self):
