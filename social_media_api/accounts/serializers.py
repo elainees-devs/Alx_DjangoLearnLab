3e933171618f78749149c1b65d6b6a668043aaea
elainees-devs <elainees.devs@gmail.com>
2025-10-06 20:18:57 +0300

    feat(accounts): add token generation in RegisterSerializer using create_user and Token.objects.get_or_create

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,36 +1,38 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth.password_validation import validate_password
+from rest_framework.authtoken.models import Token
 
-from django.contrib.auth.models import User
-
-
-User=get_user_model()
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'bio', 'profile_picture']   
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'bio', 'profile_picture']
         read_only_fields = ['id']
 
-    class RegisterSerializer(serializers.ModelSerializer):
-        password = serializers.CharField(write_only=True, required=True, validators=[validate_password] )
-        password2 = serializers.CharField(write_only=True, required=True)
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    token = serializers.CharField(read_only=True)  # Include token in response
 
-        class Meta:
-            model = User
-            fields = ['username', 'email', 'password', 'password2','first_name', 'last_name']
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2', 'first_name', 'last_name', 'token')
 
-        def validate(self, attrs):
-            if attrs.get['password'] != attrs['password2']:
-                raise serializers.ValidationError({"password": "Password fields didn't match."})
-            return attrs
+    def validate(self, attrs):
+        if attrs.get('password') != attrs.get('password2'):
+            raise serializers.ValidationError({'password': "Password fields didn't match."})
+        return attrs
 
-        def create(self, validated_data):
-            validated_data.pop('password2', None)
-            password= validated_data.pop('password')
-            user= User(**validated_data)
-            user.set_password(password)
-            user
-            return user
-      
+    def create(self, validated_data):
+        validated_data.pop('password2', None)
+        password = validated_data.pop('password')
+        user = User.objects.create_user(**validated_data)  # calls CustomUser manager
+        user.set_password(password)
+        user.save()
+        # Create token for the new user
+        token, _ = Token.objects.get_or_create(user=user)
+        # Add token to serializer response
+        self.fields['token'] = serializers.CharField(read_only=True, default=token.key)
+        return user
