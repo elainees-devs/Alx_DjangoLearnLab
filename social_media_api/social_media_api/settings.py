fe7c7298cae5bf690e2e4b5b6c516cdd2b213c27
elainees-devs <elainees.devs@gmail.com>
2025-10-07 12:50:21 +0300

    chore: make DEBUG configurable via environment variable

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -16,8 +16,8 @@ environ.Env.read_env(env_file=BASE_DIR/'.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Set DEBUG dynamically
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['localhost']
 
