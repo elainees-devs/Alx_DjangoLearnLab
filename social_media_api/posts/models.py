909aa801788df88e5dcaaf2a4d332c13b57747d4
elainees-devs <elainees.devs@gmail.com>
2025-10-07 09:48:38 +0300

    feat(posts): add Post and Comment models with relations to User

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User=settings.AUTH_USER_MODEL
+
+class Post(models.Model):
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    title=models.CharField(max_length=120)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=['-created_at']
+
+    def __str__(self):
+        return f"{self.title} by {self.user.author}"
+    
+class Comment(models.Model):
+    post=models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author=models.ForeignKey(User, on_delete=models.CASCADE)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=['created_at']
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post_id}"
