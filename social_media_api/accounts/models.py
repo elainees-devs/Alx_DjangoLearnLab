7ac7d8df3cdd04b0d4575de362c3f13d787070f5
elainees-devs <elainees.devs@gmail.com>
2025-10-07 12:23:51 +0300

    feat(accounts): add following/followers ManyToMany and helper methods to CustomUser

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,7 +6,20 @@ from django.db import models
 class CustomUser(AbstractUser):
     bio=models.TextField(blank=True)
     profile_picture=models.ImageField(upload_to='profile_picture/', blank=True, null=True)
-    followers=models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    following=models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
+
+    def follow(self, user):
+        if user == self:
+            return
+        self.following.add(user)
+
+    def unfollow(self, user):
+        if user == self:
+            return
+        self.following.remove(user)
+
+    def is_following(self, user):
+        return self.following.filter(pk=user.pk).exists()
 
     def __str__(self):
         return self.username
