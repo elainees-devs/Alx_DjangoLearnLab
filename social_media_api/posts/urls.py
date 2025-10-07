69cffa4207c99c5276a6650f57de19b2cfdd8e1d
elainees-devs <elainees.devs@gmail.com>
2025-10-07 15:02:01 +0300

    feat(urls): add routes for liking and unliking posts

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, FeedAPIView
+from .views import PostViewSet, CommentViewSet, FeedAPIView, LikePostView, UnlikePostView
 
 app_name = "posts"
 
@@ -11,4 +11,6 @@ router.register(r'comments', CommentViewSet, basename='comment')
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedAPIView.as_view(), name='feed'),  # <- feed endpoint
+    path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
