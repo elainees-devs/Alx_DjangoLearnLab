b0654bd5cfbaa779408e51c2caffd6cfe419d631
elainees-devs <elainees.devs@gmail.com>
2025-10-08 09:20:45 +0300

    test(notifications): update NotificationTests to use Token auth and CustomUser

--- social_media_api/notifications/tests.py
+++ social_media_api/notifications/tests.py
@@ -1,33 +1,28 @@
 from rest_framework.test import APITestCase
-from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 from django.urls import reverse
+from accounts.models import CustomUser
 from posts.models import Post
-from notifications.models import Notification
 from notifications.utils import create_notification
-
-User = get_user_model()
 
 class NotificationTests(APITestCase):
     def setUp(self):
-        self.user1 = User.objects.create_user(username='user1', password='pass123')
-        self.user2 = User.objects.create_user(username='user2', password='pass123')
-        self.post = Post.objects.create(author=self.user2, content='Hello World')
-        self.url = reverse('notifications')
-        self.client.login(username='user2', password='pass123')
+        self.user1 = CustomUser.objects.create_user(username='user1', password='pass12345')
+        self.user2 = CustomUser.objects.create_user(username='user2', password='pass12345')
+        self.post = Post.objects.create(author=self.user2, content="Hello World")
+
+        # Create token for user2 (the one fetching notifications)
+        self.token = Token.objects.create(user=self.user2)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.notifications_url = reverse('notifications')
 
     def test_create_notification(self):
-        """Test creating a notification via helper"""
         create_notification(actor=self.user1, recipient=self.user2, verb='liked your post', target=self.post)
-        self.assertEqual(Notification.objects.count(), 1)
-        notif = Notification.objects.first()
-        self.assertEqual(notif.recipient, self.user2)
-        self.assertEqual(notif.actor, self.user1)
-        self.assertEqual(notif.verb, 'liked your post')
+        self.assertEqual(self.user2.notifications.count(), 1)
 
     def test_fetch_notifications(self):
-        """Test fetching notifications for a user"""
         create_notification(actor=self.user1, recipient=self.user2, verb='liked your post', target=self.post)
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['verb'], 'liked your post')
+
+        response = self.client.get(self.notifications_url)
+        self.assertEqual(response.status_code, 200)   
