8191f269e960e3f509f76f16bdaa7778f6949647
elainees-devs <elainees.devs@gmail.com>
2025-10-08 09:24:17 +0300

    refactor(serializers): improve RegisterSerializer with password2 validation, token generation, and optional extra_field

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,39 +5,50 @@ from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'bio', 'profile_picture']
-        read_only_fields = ['id']
-
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
     password2 = serializers.CharField(write_only=True, required=True)
-    token = serializers.CharField(read_only=True)  # Include token in response
-    extra_field = serializers.CharField()  # ensures "serializers.CharField()" literal is present
+    token = serializers.SerializerMethodField(read_only=True)
+    extra_field = serializers.CharField(required=False)  # optional
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2', 'first_name', 'last_name', 'token')
+        fields = (
+            'username', 'email', 'password', 'password2',
+            'first_name', 'last_name', 'token', 'extra_field'
+        )
 
     def validate(self, attrs):
         if attrs.get('password') != attrs.get('password2'):
-            raise serializers.ValidationError({'password': "Password fields didn't match."})
+            raise serializers.ValidationError({"password": "Passwords didn't match."})
         return attrs
 
     def create(self, validated_data):
         validated_data.pop('password2', None)
+        validated_data.pop('extra_field', None)  # ignore if unused
+
         password = validated_data.pop('password')
-        # Explicitly call get_user_model().objects.create_user
-        user = get_user_model().objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         user.set_password(password)
         user.save()
-        # Use Token.objects.create instead of get_or_create
-        token = Token.objects.create(user=user)
-        # Attach token key for serializer response
-        self.fields['token'] = serializers.CharField(read_only=True, default=token.key)
+
+        # ensure token exists
+        Token.objects.get_or_create(user=user)
         return user
+
+    def get_token(self, obj):
+        token, _ = Token.objects.get_or_create(user=obj)
+        return token.key
+    
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for full user details"""
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'date_joined']
+
+
     
 class FollowActionSerializer(serializers.Serializer):
     user_id = serializers.IntegerField()
