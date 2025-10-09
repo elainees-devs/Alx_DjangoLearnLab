0a8d0d3f3e16d2fc049e73725b9dfa95d3b8b40e
elainees-devs <elainees.devs@gmail.com>
2025-10-09 14:23:39 +0300

    chore: configure STATIC_ROOT for production using os.path

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 import environ
 
 
@@ -161,6 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where collectstatic will gather all static files
 
 # For media uploads (profile pictures)
 MEDIA_URL = '/media/'
