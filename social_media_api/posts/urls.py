5b8704c78d1454507cf8d0b4d1c626b670869608
elainees-devs <elainees.devs@gmail.com>
2025-10-09 11:49:20 +0300

    feat(posts): add add_comment endpoint to posts URLs

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, FeedAPIView, LikePostView, UnlikePostView
+from .views import PostViewSet, CommentViewSet, FeedAPIView, LikePostView, UnlikePostView, add_comment
 
 app_name = "posts"
 
@@ -10,7 +10,8 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('feed/', FeedAPIView.as_view(), name='feed'),  # <- feed endpoint
+    path('feed/', FeedAPIView.as_view(), name='feed'), 
     path('<int:pk>/like/', LikePostView.as_view(), name='like-post'),
     path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
+    path('add-comment/<int:post_id>/', add_comment, name='add_comment'),
 ]
