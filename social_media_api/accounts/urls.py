1ab233cccba9fee96d91583a63c5562d6a38423c
elainees-devs <elainees.devs@gmail.com>
2025-10-09 11:41:07 +0300

    refactor(urls): replace RegisterAPIView & CustomObtainAuthToken with RegisterUser, LoginUser, LogoutUser and add app_name for namespacing

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import RegisterAPIView, CustomObtainAuthToken, ProfileAPIView
+from .views import ProfileAPIView, RegisterUser, LoginUser, LogoutUser
 from . import views
 
+app_name = "accounts" # Added app_name for namespacing
+
 urlpatterns=[
-    path('register/', RegisterAPIView.as_view(), name='register'),
-    path('login/', CustomObtainAuthToken.as_view(), name='login'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', LogoutUser.as_view(), name='logout'),
     path('profile/', ProfileAPIView.as_view(), name='profile'),
     path('follow/<int:user_id>/', views.FollowUserAPIView.as_view(), name='follow-user'),
     path('unfollow/<int:user_id>/', views.UnfollowUserAPIView.as_view(), name='unfollow-user'),
